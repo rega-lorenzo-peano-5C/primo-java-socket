166a859cc59a524e3dd5a371336988d94db1ddbe
rega-lorenzo-peano-5C <56298421+rega-lorenzo-peano-5C@users.noreply.github.com>
2019-10-08 11:55:40 +0200

    Add files via upload

---
+++ PrimoClient/src/primoclient/PrimoClient.java
@@ -0,0 +1,34 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package primoclient;
+
+import java.io.IOException;
+import java.net.Socket;
+import java.util.logging.Logger;
+
+/**
+ *
+ * @author pc15
+ */
+public class PrimoClient {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        try{
+            Socket s=new Socket ("169.254.236.85", 2400);
+            
+            s.getOutputStream().write(90);
+            s.close();
+            
+        } catch (IOException ex){
+            Logger.getLogger(string)
+        }
+    }
+    
+}
